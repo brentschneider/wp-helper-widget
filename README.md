0ee9a8ff1a2904d2fbb394d78f97eba229864f88
Brent Schneider <bschneider@govt108325wp.eastern.ewu.edu>
2017-11-01 10:01:57 -0700

    switching to EWU awesome font, adding request info line item

--- README.md
+++ README.md
@@ -1,1 +1,69 @@
-# wp-helper-widget
+# Persistant Widget
+
+
+Contributors:      brentschneiuder
+Tags:              metaboxes, widget, chat, persistant widget
+Requires at least: Wordpress 4+
+Tested up to:      4.8.2
+License:           GPLv2 or later
+License URI:       http://www.gnu.org/licenses/gpl-2.0.html
+
+== Description ==
+
+Persistant widget adds a widget area to right hand side of the page.
+
+
+# Adding Chat (Chatra set up)
+
+
+### A. Set up for wordpress
+
+TL;DR
+
+1. log into your Wordpress admin panel and go to Plugins → Add new.
+2. Enter “chatra” into Search plugins field.
+3. Install and activate plugin.
+4. Go to Settings → Chatra chat.
+5. Log in into app.chatra.io in another browser tab and copy the widget code from Set up & customize section.
+6. Go back to Wordpress console, paste the code and press Save changes.
+
+
+This is your widget snippet. Copy-paste it into the code of the pages you want to chat on. Right before the </head> tag is the best option.
+To get yours, log into chatra and click on [set up & customize](https://app.chatra.io/settings/general).
+
+```
+<!-- Chatra {literal} -->
+<script>
+    (function(d, w, c) {
+        w.ChatraID = 'qwerty8765309';
+        var s = d.createElement('script');
+        w[c] = w[c] || function() {
+            (w[c].q = w[c].q || []).push(arguments);
+        };
+        s.async = true;
+        s.src = (d.location.protocol === 'https:' ? 'https:': 'http:')
+        + '//call.chatra.io/chatra.js';
+        if (d.head) d.head.appendChild(s);
+    })(document, window, 'Chatra');
+</script>
+<!-- /Chatra {/literal} -->
+```
+
+
+### B. Add the Javascript script to page. 
+
+Chatra Javascript [referance](https://chatra.io/help/api/#api-reference)
+```
+<script>
+window.ChatraSetup = {
+    chatHeight: 350,
+    colors: {
+        buttonText: '#f600a5',
+        buttonBg: '#fff'
+    },
+    startHidden: true
+};
+</script>
+
+<!-- Chatra widget code -->
+```
---
+++ persistant-widgets.php
@@ -0,0 +1,146 @@
+<?php
+/*
+  Plugin Name: Persistant Widget
+  Plugin URI: https://github.com/brentschneider/wp-helper-widget
+  Description: Adds persistant helper widget(s) to aid the user
+  Version: 0.2
+  Date: Oct/30/17
+  Author: Brent Schneider
+  Author URI: http://brentschneider.github.io
+ */
+
+class Persistant_Widgets extends WP_Widget {
+
+    function __construct() {
+        parent::__construct(
+                'persistant_widgets',
+                __('Persistant Widget', 'translation_domain'), // Name
+                array('description' => __('Persistant Widget', 'translation_domain'),)
+        );
+    }
+
+
+    /**************************************************************
+     * Front-end display of widget.
+     */
+
+    public function widget( $args, $instance ) {
+
+        $chat_title = $instance['chat_title'];
+        $chat_url = $instance['chat_url'];
+        $info_title = $instance['info_title'];
+        $info_url = $instance['info_url'];
+        $donation_title = $instance['donation_title'];
+        $donation_url = $instance['donation_url'];
+        
+
+        // HTML output section
+
+        $chat_profile = '<li><a onclick="' . $chat_url . '" href="#" class="ea-speech-bubble">' . $chat_title . '</a></li>';
+        $info_profile = '<li><a href="' . $info_url . '" target="_self" class="ea-info" aria-hidden="true">' . $info_title .'</a></li>';
+        $donation_profile = '<li><a href="' . $donation_url . '" target="_self" class="ea-gift">' . $donation_title .'</a></li>';
+
+        // Builds the widget on the page
+
+        echo $args['before_widget'];
+
+            echo '<div id="persistant_widget__actions" class="call_to_action"><ul>';
+
+                echo (!empty($chat_url) ) ? $chat_profile : null;
+                echo (!empty($info_url) ) ? $info_profile : null;
+                echo (!empty($donation_url) ) ? $donation_profile : null;
+
+            echo '</ul></div>';
+
+        echo $args['after_widget'];
+    }
+
+
+
+    /**************************************************************
+     * Back-end widget form.
+     * @see WP_Widget::form()
+     */
+
+    public function form($instance) {
+
+        // empty($instance['chat_title']) ? $chat_title = 'How can we help?' : null;
+
+        isset($instance['chat_title']) ? $chat_title = $instance['chat_title'] : null;
+        isset($instance['chat_url']) ? $chat_url = $instance['chat_url'] : null;
+
+        isset($instance['info_title']) ? $info_title = $instance['info_title'] : null;
+        isset($instance['info_url']) ? $info_url = $instance['info_url'] : null;
+
+        isset($instance['donation_title']) ? $donation_title = $instance['donation_title'] : null;
+        isset($instance['donation_url']) ? $donation_url = $instance['donation_url'] : null;
+
+        ?>
+
+        <!-- // Chat -->
+        <p>
+            <label for="<?php echo $this->get_field_id('chat_title'); ?>"><?php _e('Chat Title:'); ?></label> <br />
+            <input class="" id="<?php echo $this->get_field_id('chat_title'); ?>" name="<?php echo $this->get_field_name('chat_title'); ?>" type="text" value="<?php echo esc_attr($chat_title); ?>"><br />
+            <label for="<?php echo $this->get_field_id('chat_url'); ?>"><?php _e('Chat URL:'); ?></label><br />
+            <input class="" id="<?php echo $this->get_field_id('chat_url'); ?>" name="<?php echo $this->get_field_name('chat_url'); ?>" type="text" value="<?php echo esc_attr($chat_url); ?>">
+        </p>
+
+        <!-- // Info -->
+        <p>
+            <label for="<?php echo $this->get_field_id('info_title'); ?>"><?php _e('Info Title:'); ?></label> <br />
+            <input class="" id="<?php echo $this->get_field_id('info_title'); ?>" name="<?php echo $this->get_field_name('info_title'); ?>" type="text" value="<?php echo esc_attr($info_title); ?>"><br />
+            <label for="<?php echo $this->get_field_id('info_url'); ?>"><?php _e('Info URL:'); ?></label><br />
+            <input class="" id="<?php echo $this->get_field_id('info_url'); ?>" name="<?php echo $this->get_field_name('info_url'); ?>" type="text" value="<?php echo esc_attr($info_url); ?>">
+        </p>
+
+        <!-- // Docation -->
+        <p>
+            <label for="<?php echo $this->get_field_id('donation_title'); ?>"><?php _e('Donation Title:'); ?></label><br />
+            <input class="" id="<?php echo $this->get_field_id('donation_title'); ?>" name="<?php echo $this->get_field_name('donation_title'); ?>" type="text" value="<?php echo esc_attr($donation_title); ?>"><br />
+            <label for="<?php echo $this->get_field_id('donation_url'); ?>"><?php _e('Donation URL:'); ?></label><br />
+            <input class="" id="<?php echo $this->get_field_id('donation_url'); ?>" name="<?php echo $this->get_field_name('donation_url'); ?>" type="text" value="<?php echo esc_attr($donation_url); ?>">
+        </p>
+
+        <?php
+    }
+
+
+    /**************************************************************
+     * Sanitize widget form values as they are saved.
+     * @see WP_Widget::update()
+     * @param array $new_instance Values just sent to be saved.
+     * @param array $old_instance Previously saved values from database.
+     * @return array Updated safe values to be saved.
+     */
+
+    public function update($new_instance, $old_instance) {
+
+        $instance = array();
+
+            $instance['chat_title'] = (!empty($new_instance['chat_title']) ) ? strip_tags($new_instance['chat_title']) : '';
+            $instance['chat_url'] = (!empty($new_instance['chat_url']) ) ? strip_tags($new_instance['chat_url']) : '';
+            
+            $instance['info_title'] = (!empty($new_instance['info_title']) ) ? strip_tags($new_instance['info_title']) : '';
+            $instance['info_url'] = (!empty($new_instance['info_url']) ) ? strip_tags($new_instance['info_url']) : '';
+
+            $instance['donation_title'] = (!empty($new_instance['donation_title']) ) ? strip_tags($new_instance['donation_title']) : '';
+            $instance['donation_url'] = (!empty($new_instance['donation_url']) ) ? strip_tags($new_instance['donation_url']) : '';
+
+        return $instance;
+    }
+
+}
+
+// register the widget
+function register_persistant_widgets() {
+    register_widget('persistant_widgets');
+}
+
+// enqueue css stylesheet
+function persistant_widgets_css() {
+    wp_enqueue_style('social-profile-widget', plugins_url('persistant-widgets.css', __FILE__));
+}
+
+// And 1, 2, 3 ... go!
+add_action('widgets_init', 'register_persistant_widgets');
+add_action('wp_enqueue_scripts', 'persistant_widgets_css');
